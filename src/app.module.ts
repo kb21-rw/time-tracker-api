dd2f47b9656f979085d49863c3e585eb0080709b
Gasorekibo <m.gasore@alustudent.com>
2025-02-17 14:54:37 +0200

    feat: time tracker api project initialization

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+
+import { User } from './user/entities/user.entity'
+import { UserModule } from './user/user.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: process.env.DB_TYPE as any,
+        host: process.env.DB_HOST,
+        port: Number.parseInt(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        entities: [User],
+        synchronize: true,
+
+        ssl:
+          process.env.DB_SSL_MODE === 'require'
+            ? { rejectUnauthorized: false }
+            : false,
+      }),
+    }),
+    UserModule,
+  ],
+})
+export class AppModule {}
