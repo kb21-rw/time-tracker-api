a02038be577c881086f0643a5619ccaeeb22468d
manzitresor <manzitre@gmail.com>
2025-02-26 16:02:33 +0200

    chore: add user entity and user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsEmail, MinLength, minLength } from "class-validator"
+
+
+export enum UserRole {
+    Admin = 'Admin',
+    Member = 'Member'
+}
+
+export class CreateUserDto {
+    id: string
+
+    @ApiProperty({
+        example: 'Christelle',
+        required: true
+    })
+    name: string
+
+    @ApiProperty({
+        example: 'christelle@gmail.com',
+        required: true
+    })
+    @IsEmail()
+    email: string
+
+    @ApiProperty({
+        example: 'flow@123',
+        required: true
+    })
+    @MinLength(8)
+    password: string
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class User {
+    @PrimaryColumn()
+    id: number
+
+    @Column()
+    names: string
+
+    @Column()
+    email: string
+
+    @Column()
+    password: string
+}
