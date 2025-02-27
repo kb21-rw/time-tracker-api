ba85dd96e7155ba8fc90fdba02b568100effc777
Gihozo23 <christellegihozo23@gmail.com>
2025-02-27 20:37:51 +0200

    feat: update file for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { User } from './user/entities/user.entity'
 import { UserModule } from './user/user.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
+    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     TypeOrmModule.forRootAsync({
       useFactory: () => ({
-        type: process.env.DB_TYPE as any,
+        type: process.env.DB_TYPE as 'postgres',
         host: process.env.DB_HOST,
-        port: Number.parseInt(process.env.DB_PORT),
+        port: parseInt(process.env.DB_PORT),
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
