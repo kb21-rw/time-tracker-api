942202fea6d99f4460fe4d6dc165c665ca1c9032
Gasorekibo <m.gasore@alustudent.com>
2025-02-28 12:00:18 +0200

    refactor: change the mock username to email

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,11 +5,11 @@ export type User = any
 @Injectable()
 export class UsersService {
   private readonly users = [
-    { userId: 1, username: 'john', password: 'changeme' },
-    { userId: 2, username: 'maria', password: 'guess' },
+    { userId: 1, email: 'john@gmail.com', password: 'changeme' },
+    { userId: 2, email: 'maria@gmail.com', password: 'guess' },
   ]
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username)
+  async findOne(email: string): Promise<User | undefined> {
+    return this.users.find(user => user.email === email)
   }
 }
