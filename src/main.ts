e6a8e6149a89d08887b24834d168305f374bcba1
Gihozo23 <christellegihozo23@gmail.com>
2025-03-05 20:09:12 +0200

    feature: enable cors for frontend integration

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options)
   SwaggerModule.setup('api-docs', app, document)
 
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  })
   await app.listen(3000)
 }
 bootstrap()
