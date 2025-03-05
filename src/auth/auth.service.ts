0833770a0117e1f07217f47e2d4bf8d9f3f26954
Gasorekibo <m.gasore@alustudent.com>
2025-03-05 15:41:17 +0200

    feat: user login functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,30 +1,51 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common'
-import { UsersService } from '../users/users.service'
 import { JwtService } from '@nestjs/jwt'
-
+import { User } from 'src/users/entities/user.entity'
+import * as bcrypt from 'bcrypt'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 @Injectable()
 export class AuthService {
   constructor(
-    private usersService: UsersService,
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
     private jwtService: JwtService,
   ) {}
 
-  async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.usersService.findByEmail(email)
-    if (user && user.password === pass) {
-      const { password, ...result } = user
-      return result
+  async validateUser(
+    email: string,
+    password: string,
+  ): Promise<Partial<User> | UnauthorizedException> {
+    const user = await this.userRepository.findOne({ where: { email } })
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...userWithNoPassword } = user
+      return userWithNoPassword
+    } else {
+      return new UnauthorizedException('Invalid Credentials')
     }
-    return null
   }
-  async login(loginDto: { email: string; password: string }) {
-    const user = await this.validateUser(loginDto.email, loginDto.password)
-    if (!user) {
-      throw new UnauthorizedException('Invalid email or password')
+  async login(user: Partial<User>): Promise<{
+    user: Partial<User>
+    access_token: string
+  }>
+ 
+  {
+    const payload = {
+      email: user?.email,
+      role: user?.roles,
+      id: user?.id,
     }
-    const payload = { email: user.email, sub: user.id }
+    if (!user?.id) {
+      throw new UnauthorizedException('Invalid user data')
+    }
+
+    const { password, ...userWithoutPassword } = user
+
     return {
-      access_token: this.jwtService.sign(payload),
+      user: userWithoutPassword,
+      access_token: this.jwtService.sign(payload, {
+        secret: process.env.JWT_SECRET,
+      }),
     }
   }
 }
