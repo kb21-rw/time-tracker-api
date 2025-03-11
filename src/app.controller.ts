020c6e491614fb49b8cd828b29685910fcc08cc5
manzitresor <manzitre@gmail.com>
2025-03-11 16:02:40 +0200

    fix: remove auth in app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,21 +9,9 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard'
 export class AppController {
   constructor(
     private readonly appService: AppService,
-    private authService: AuthService,
   ) {}
   @Get()
   getHello(): string {
     return this.appService.getHello()
-  }
-  @UseGuards(LocalAuthGuard)
-  @Post('auth/login')
-  async login(@Request() req) {
-    return this.authService.login(req.user)
-  }
-
-  @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user
   }
 }
