9b2d9f201e95d4525ffd3f84b213111288a45edd
Gihozo23 <christellegihozo23@gmail.com>
2025-03-19 20:47:42 +0200

    refactor: update the user entity for junction table

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,28 +1,38 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
-import { UserRole } from "src/util/role.enum";
-import { Exclude } from "class-transformer";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm'
+import { UserRole } from 'src/util/role.enum'
+import { Exclude } from 'class-transformer'
+import { UserWorkspace } from '../../workspaces/entities/user-workspace.entity'
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number
+  @PrimaryGeneratedColumn()
+  id: number
 
-    @Column()
-    fullName: string
+  @Column()
+  fullName: string
 
-    @Column({unique: true})
-    email: string
+  @Column({ unique: true })
+  email: string
 
-    @Column()
-    @Exclude()
-    password: string
+  @Column()
+  @Exclude()
+  password: string
 
-    @Column()
-    roles: UserRole
+  @Column()
+  roles: UserRole
 
-    @CreateDateColumn()
-    created_at: Date
+  @CreateDateColumn()
+  created_at: Date
 
-    @UpdateDateColumn()
-    updated_at: Date
+  @UpdateDateColumn()
+  updated_at: Date
+  @OneToMany(() => UserWorkspace, userWorkspace => userWorkspace.user)
+  userWorkspaces: UserWorkspace[]
 }
