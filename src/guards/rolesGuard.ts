b21d8dd9b94899a70ff8349c0c64ac5c9fc90b7d
Gihozo23 <christellegihozo23@gmail.com>
2025-03-24 23:06:11 +0200

    feature: implement the rolesGuard

---
+++ src/guards/rolesGuard.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common'
+import { UserRole } from 'src/util/role.enum'
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest()
+    const user = request.user
+
+    if (user.roles !== UserRole.ADMIN) {
+      throw new ForbiddenException(`Dear user, you can't create a workspace`)
+    }
+
+    return true
+  }
+}
