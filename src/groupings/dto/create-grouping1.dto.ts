25aff32a4672562205279430a5d1cd7ada9de68b
Gihozo23 <christellegihozo23@gmail.com>
2025-04-17 16:17:01 +0200

    refactor: add an example in the createGrouping1DTO

--- src/groupings/dto/create-grouping1.dto.ts
+++ src/groupings/dto/create-grouping1.dto.ts
@@ -1,6 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsNotEmpty, IsString } from 'class-validator'
 
 export class CreateGrouping1Dto {
+  @ApiProperty({
+    example: 'The gym',
+    required: true,
+  })
   @IsNotEmpty()
   @IsString()
   name: string
