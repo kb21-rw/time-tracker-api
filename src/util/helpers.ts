a32dd18756a8f603c728cee24f4ded67567a1935
Gihozo23 <christellegihozo23@gmail.com>
2025-04-17 16:17:54 +0200

    feat: create a helper for verifying entities existance

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -1,5 +1,7 @@
 import { ConflictException, NotFoundException } from '@nestjs/common'
+import { Grouping1 } from 'src/groupings/entities/grouping1.entity'
 import { UserWorkspace } from 'src/workspaces/entities/user-workspace.entity'
+import { Workspace } from 'src/workspaces/entities/workspace.entity'
 export function verifyIfNameNotTaken(userWorkspace?: UserWorkspace) {
   if (!userWorkspace || !userWorkspace.workspace) return
 
@@ -9,6 +11,19 @@ export function verifyIfNameNotTaken(userWorkspace?: UserWorkspace) {
   if (existingName) {
     throw new ConflictException(
       `A workspace with the name ${existingName} already exists`,
+    )
+  }
+}
+export function VerifyIfEntityExists(
+  workspace?: Workspace,
+  grouping1?: Grouping1,
+) {
+  if (!workspace) {
+    throw new NotFoundException('Workspace not found')
+  }
+  if (grouping1) {
+    throw new ConflictException(
+      `Grouping 1 with the name ${grouping1.name} already exists`,
     )
   }
 }
