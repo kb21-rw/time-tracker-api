a873843818d84ca5f68b60913a8fcfa4a826bd19
Gihozo23 <christellegihozo23@gmail.com>
2025-04-23 22:09:20 +0200

    refactor: rename grouping1 module to clients module

--- src/groupings/clients.module.ts
+++ src/groupings/clients.module.ts
@@ -1,21 +1,21 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { Grouping1Service } from './grouping1.service'
-import { Grouping1Controller } from './grouping1.controller'
+import { ClientsService } from './clients.service'
+import { ClientsController } from './clients.controller'
 import { Workspace } from 'src/workspaces/entities/workspace.entity'
 import { AuthModule } from 'src/auth/auth.module'
-import { Grouping1 } from '../entities/grouping1.entity'
+import { Client } from './entities/client.entity'
 import { UserWorkspace } from 'src/workspaces/entities/user-workspace.entity'
 import { WorkspacesModule } from 'src/workspaces/workspaces.module'
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Grouping1, Workspace, UserWorkspace]),
+    TypeOrmModule.forFeature([Client, Workspace, UserWorkspace]),
     WorkspacesModule,
     AuthModule,
   ],
-  controllers: [Grouping1Controller],
-  providers: [Grouping1Service],
+  controllers: [ClientsController],
+  providers: [ClientsService],
   exports: [],
 })
-export class Grouping1Module {}
+export class ClientsModule {}
