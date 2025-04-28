60b0ccb646a9b4655e8e57a0cc43b221860fd9c4
Gihozo23 <christellegihozo23@gmail.com>
2025-04-28 15:01:22 +0200

    refactor: update the workspacePermission guard to work with decorators

--- src/guards/workspacePermission.guard.ts
+++ src/guards/workspacePermission.guard.ts
@@ -1,33 +1,49 @@
-import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { UserRole } from "src/util/role.enum";
-import { UserWorkspace } from "src/workspaces/entities/user-workspace.entity";
-import { Repository } from "typeorm";
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from '@nestjs/common'
+import { Reflector } from '@nestjs/core'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { UserWorkspace } from 'src/workspaces/entities/user-workspace.entity'
+import { UserRole } from 'src/util/role.enum'
+import { WORKSPACE_ROLES_KEY } from '../decorators/workspace-roles.decorator'
 
 @Injectable()
 export class WorkspacePermissionGuard implements CanActivate {
   constructor(
-        @InjectRepository(UserWorkspace)
-        private userWorkspaceRepository: Repository<UserWorkspace>,
+    private readonly reflector: Reflector,
+    @InjectRepository(UserWorkspace)
+    private readonly userWorkspaceRepository: Repository<UserWorkspace>,
   ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const userId = request.user.id; 
+    const expectedRoles = this.reflector.get<UserRole[]>(
+      WORKSPACE_ROLES_KEY,
+      context.getHandler(),
+    )
+    if (!expectedRoles || expectedRoles.length === 0) {
+      throw new ForbiddenException('No permission metadata found')
+    }
+    const request = context.switchToHttp().getRequest()
+    const userId = request.user.id
     const workspaceId = request.params.id
 
     const userWorkspace = await this.userWorkspaceRepository.findOne({
-      where: { 
-        userId: String(userId), 
+      where: {
+        userId: String(userId),
         workspaceId: workspaceId,
-        role: UserRole.ADMIN
-      }
-    });
+      },
+    })
 
-    if (!userWorkspace) {
-      throw new ForbiddenException('You do not have permission to perform this action');
+    if (!userWorkspace || !expectedRoles.includes(userWorkspace.role)) {
+      throw new ForbiddenException(
+        'You do not have permission to perform this action',
+      )
     }
-    
-    return true;
+
+    return true
   }
 }
