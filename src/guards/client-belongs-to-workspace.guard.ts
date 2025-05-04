6721ce17e8995a7ee7f6aa558332cf25d796bdc7
Gihozo23 <christellegihozo23@gmail.com>
2025-05-04 19:12:31 +0200

    feat: create a guard to check if a client belongs to a workspace

---
+++ src/guards/client-belongs-to-workspace.guard.ts
@@ -0,0 +1,36 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Client } from 'src/clients/entities/client.entity'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class ClientBelongsToWorkspaceGuard implements CanActivate {
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientRepository: Repository<Client>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const { workspaceId } = request.params
+    const { clientId } = request.body
+
+    const client = await this.clientRepository.findOne({
+      where: { id: clientId },
+      relations: ['workspace'],
+    })
+
+    if (!client || client.workspace.id !== workspaceId) {
+      throw new ForbiddenException(
+        'The provided client does not belong to this workspace',
+      )
+    }
+
+    return true
+  }
+}
