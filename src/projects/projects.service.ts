b59c95e844cf78cafdbc09e6c92a6a0296252cd5
Gihozo23 <christellegihozo23@gmail.com>
2025-05-12 21:39:39 +0200

    refactor: make checks for duplicates reusable in projects

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,24 +1,32 @@
 import { InjectRepository } from '@nestjs/typeorm'
 import { Project } from './entities/project.entity'
 import { Repository } from 'typeorm'
-import { checkIfProjectExists } from 'src/util/helpers'
 import { CreateProjectDto } from './dto/create-project.dto'
+import { ConflictException } from '@nestjs/common'
 
 export class ProjectsService {
   constructor(
     @InjectRepository(Project)
     private readonly projectRepository: Repository<Project>,
   ) {}
-  async create({ name }: CreateProjectDto, clientId: string): Promise<Project> {
-    const existingProject = await this.projectRepository.findOne({
-      where: {
-        name,
-        client: { id: clientId },
-      },
-      relations: ['client'],
-    })
 
-    checkIfProjectExists(existingProject)
+  async findByName(clientId: string, name: string): Promise<Project | null> {
+    return this.projectRepository
+      .createQueryBuilder('project')
+      .where('project.client.id = :clientId', { clientId })
+      .andWhere('LOWER(project.name) = LOWER(:name)', { name: name.trim() })
+      .getOne()
+  }
+
+  private async checkIfExists(clientId: string, name: string) {
+    const existingProject = await this.findByName(clientId, name)
+    if (!existingProject) return
+    throw new ConflictException(
+      `Project with the name ${existingProject.name} already exists`,
+    )
+  }
+  async create({ name }: CreateProjectDto, clientId: string): Promise<Project> {
+    await this.checkIfExists(clientId, name)
 
     const newProject = this.projectRepository.create({
       name,
