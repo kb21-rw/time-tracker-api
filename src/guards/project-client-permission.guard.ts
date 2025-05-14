963979e9bc2fb6c8a3a7c744183621f1ecbc05a6
Gihozo23 <christellegihozo23@gmail.com>
2025-05-14 16:03:55 +0200

    feat: implement the project-client-permission logic

---
+++ src/guards/project-client-permission.guard.ts
@@ -0,0 +1,42 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { isUUID } from 'class-validator'
+import { Project } from 'src/projects/entities/project.entity'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class ProjectClientPermissionGuard implements CanActivate {
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: Repository<Project>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest()
+    const { clientId, projectId } = request.params
+
+    if (!isUUID(projectId)) {
+      throw new ForbiddenException('Invalid projectId format')
+    }
+
+    const project = await this.projectRepository.findOne({
+      where: { id: projectId },
+      relations: ['client'],
+    })
+
+    if (!project || project.client.id !== clientId) {
+      console.log('Client ID:', clientId)
+      console.log('Project Client ID:', project.client.id)
+      throw new ForbiddenException(
+        'The provided project does not belong to the specified client',
+      )
+    }
+
+    return true
+  }
+}
