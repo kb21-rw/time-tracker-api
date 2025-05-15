41f4b177a5e8e7e788f20d1716f79b0c9d3c9336
Christelle Gihozo <109101706+Gihozo23@users.noreply.github.com>
2025-05-15 15:54:39 +0200

    Revert "Sprint 4 Staging to main"

--- src/util/helpers.ts
+++
@@ -1,2 +0,0 @@
-// A helper function is a reusable piece of code that performs a specific operation
-// not tied to a single scope, often used when the same logic is needed in multiple places i.e different services
