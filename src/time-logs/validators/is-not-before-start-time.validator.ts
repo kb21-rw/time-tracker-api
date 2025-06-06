2477b28c0cbe75181812b5198ecf8f3e67f6f4a7
Gihozo23 <christellegihozo23@gmail.com>
2025-06-06 22:10:40 +0200

    refactor: update the isNotBeforeStartTime validator

---
+++ src/time-logs/validators/is-not-before-start-time.validator.ts
@@ -0,0 +1,29 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidationArguments,
+} from 'class-validator'
+
+export function IsNotBeforeStartTime(
+  startTimeField: string,
+  validationOptions?: ValidationOptions,
+) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'isNotBeforeStartTime',
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      validator: {
+        validate(endTime: any, args: ValidationArguments) {
+          const startTime = (args.object as any)[startTimeField]
+          if (!endTime || !startTime) return true
+          return new Date(endTime) > new Date(startTime)
+        },
+        defaultMessage(args: ValidationArguments) {
+          return `${args.property} must be after start time`
+        },
+      },
+    })
+  }
+}
