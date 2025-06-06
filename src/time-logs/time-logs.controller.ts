284e128e032bec35cf72b38b7677c4ee0d3c8079
Gihozo23 <christellegihozo23@gmail.com>
2025-06-06 22:09:41 +0200

    feat: implement the update method in the controller

--- src/time-logs/time-logs.controller.ts
+++ src/time-logs/time-logs.controller.ts
@@ -6,6 +6,7 @@ import {
   Param,
   Req,
   Get,
+  Patch,
 } from '@nestjs/common'
 import { TimeLogsService } from './time-logs.service'
 import { StartTimeEntryDto } from './dto/start-time-entry.dto'
@@ -21,6 +22,7 @@ import { UserRole } from 'src/util/role.enum'
 import { WorkspacePermissionGuard } from 'src/guards/workspace-permission.guard'
 import { RequestWithUser } from 'src/auth/types/request-with-user'
 import { StopTimeEntryDto } from './dto/stop-time-entry.dto'
+import { UpdateTimeLogDto } from './dto/update-time-log.dto'
 
 @ApiTags('Time Logs')
 @UseGuards(JwtAuthGuard, WorkspacePermissionGuard)
@@ -151,5 +153,59 @@ export class TimeLogsController {
     @Req() req: RequestWithUser,
   ) {
     return this.timeLogsService.getAll(req.user.id, workspaceId)
+  }
+
+  @WorkspaceRoles(UserRole.ADMIN, UserRole.MEMBER)
+  @Patch(':timeLogId')
+  @ApiOperation({ summary: 'Update a time log entry' })
+  @ApiResponse({
+    status: 200,
+    description: 'Time log entry updated successfully',
+    schema: {
+      example: {
+        message: 'Time log entry updated successfully',
+        timeLog: {
+          id: '1234567890',
+          startTime: '2023-10-01T12:00:00Z',
+          endTime: '2023-10-01T14:00:00Z',
+          description: 'Updated description for project X',
+          projectId: 'project123',
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Bad Request. Missing or invalid inputs.',
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Unauthorized',
+  })
+  @ApiResponse({
+    status: 403,
+    description:
+      'Forbidden. You do not have permission to perform this action.',
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Time log entry not found',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal Server Error',
+  })
+  async update(
+    @Param('workspaceId') workspaceId: string,
+    @Param('timeLogId') timeLogId: string,
+    @Body() updateDto: UpdateTimeLogDto,
+    @Req() req: RequestWithUser,
+  ) {
+    return this.timeLogsService.update(
+      timeLogId,
+      workspaceId,
+      req.user.id,
+      updateDto,
+    )
   }
 }
