2f4c2f9229f656467315908d96a723586f4e176a
Gihozo23 <christellegihozo23@gmail.com>
2025-08-05 00:04:11 +0200

    docs: update auth API documentation with timezone

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -25,9 +25,10 @@ export class AuthController {
           fullName: 'Jackson Eric',
           email: 'jackson@gmail.com',
           roles: UserRole.ADMIN,
+          timeZone: 'Africa/Kigali',
         },
         access_token:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphY2tzb25AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwiaWQiOjMsImlhdCI6MTc0MTE4MTcyMSwiZXhwIjoxNzQxMTg1MzIxfQ.28Kz5VQUkLkD-P5LLPUUID0YahXYnHJS30HkwfQZXEA',
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImphY2tzb25AZ21haWwuY29tIiwicm9sZSI6IkFkbWluIiwiaWQiOjMsInRpbWVab25lIjoiQWZyaWNhL0tpZ2FsaSIsImlhdCI6MTc0MTE4MTcyMSwiZXhwIjoxNzQxMTg1MzIxfQ.exampleToken',
       },
     },
   })
